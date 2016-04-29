178c8b003d79b3e0e26e21fe3afcd3a18ac5a405
bgreer5050@gmail.com <bgreer5050@gmail.com>
2016-04-29 05:24:53 -0500

    Add project files.

---
+++ ContractorPortal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContractorPortal.Startup))]
+namespace ContractorPortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
