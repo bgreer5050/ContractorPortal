8a9ba0b95d68266931eb782940f1c44a973aa2be
bgreer5050@gmail.com <bgreer5050@gmail.com>
2016-05-11 15:50:39 -0500

    Added Stormdamage controller and view.

---
+++ ContractorPortal/Controllers/StormdamageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContractorPortal.Controllers
+{
+    public class StormdamageController : Controller
+    {
+        // GET: Stormdamage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
