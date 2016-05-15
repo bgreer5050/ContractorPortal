3087879a4c93a3fdd1755ffbb236c679aac08ebd
bgreer5050@gmail.com <bgreer5050@gmail.com>
2016-05-15 11:51:47 -0500

    Creates a Sqlite DB on App Start if it does not exist.  NEXT - Create necessary tables if they do not exist.

--- ContractorPortal/Global.asax.cs
+++ ContractorPortal/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -18,6 +19,35 @@ namespace ContractorPortal
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            SetUpDataBase();
+
+        }
+
+        private void SetUpDataBase()
+        {
+            //Does DB Exist
+
+            string path =  System.IO.Path.Combine(Environment.CurrentDirectory, "kgroofing.sqlite");
+            bool blnDBExist = System.IO.File.Exists(path);
+            if (blnDBExist != true)
+            {
+                System.Data.SQLite.SQLiteConnection.CreateFile("kgroofing.sqlite");
+            }
+            //throw new NotImplementedException();
+
+            string strCommandText = "CREATE TABLE requestforquote ([Id] INTEGER PRIMARY KEY, Name TEXT, Address TEXT, Note TEXT, DateTime TEXT,CONSTRAINT [sqlite_master_PK_records]);";
+            System.Data.SQLite.SQLiteConnection conn = new System.Data.SQLite.SQLiteConnection("Data Source = kgroofing.sqlite; Version = 3;");
+            System.Data.SQLite.SQLiteCommand cmd = new System.Data.SQLite.SQLiteCommand(strCommandText, conn);
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+
+            //strCommandText = "CREATE UNIQUE INDEX [sqlite_autoindex_requestforquote_1_requestforquote] ON [requestforquote] ([Id] ASC);";
+            //cmd.ExecuteNonQuery();
+
+            conn.Close();
+
         }
     }
 }
